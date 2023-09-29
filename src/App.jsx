d1f42e340c3fd62248b88b0eaa4a43762b991d85
Rohit Kumar Modi <rohitarya7478@gmail.com>
2023-09-29 21:41:04 +0530

    redux

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import AddTodo from './componants/AddTodo'
+import Todos from './componants/Todos'
+
+function App() {
+  
+  return (
+    
+    <>
+      <h1 className=' text-center mt-8 font-bold text-3xl' >Todo App</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
